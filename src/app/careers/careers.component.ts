8767571d62e623a96b2a4c926dd7261ba384bf32
David Quisenberry and Jayeson Kiyabu <jayeson@>
2017-04-04 11:50:40 -0700

    add dynamic routes to career paths

--- src/app/careers/careers.component.ts
+++ src/app/careers/careers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Router } from '@angular/router';
 import { CareerService } from './../career.service';
+import { Career } from './../career.model';
 
 
 @Component({
@@ -15,9 +16,12 @@ export class CareersComponent implements OnInit {
 
   constructor(private router: Router, private careerService: CareerService) { }
 
+  goToDetailPage(clickedCareer) {
+    this.router.navigate(['career-details', clickedCareer.$key]);
+  };
+
   ngOnInit() {
     this.careers = this.careerService.getCareers();
-
   }
 
 }
